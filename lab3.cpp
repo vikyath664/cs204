fcb7ddfcb7493296ad0c9ceaeb305d8759d8545e
vikyath664 <49971328+vikyath664@users.noreply.github.com>
2019-08-15 20:02:21 +0530

    Update lab3.cpp

--- lab3.cpp
+++ lab3.cpp
@@ -17,62 +17,90 @@ for(i=0;i<s1.length();i++)
      {a.push(s1[i]);
      continue;}
 
-  if(s1[i]=='|'&& !a.empty())
-  {x=a.top();
-  if(x=='|')
-  {a.pop();}
-  if(x!='|')
-  {a.push('|');}}
+     if(s1[i]=='|'&& !a.empty())
+     {
+          x=a.top();
+     if(x=='|')
+     {
+          a.pop();
+     }
+     if(x!='|')
+     {
+          a.push('|');
+     }
+     }
 
      if((s1[i]=='}'||s1[i]==')'||s1[i]==']'||s1[i]=='>')&& a.empty())
-     {return false;
-     break;}
+     {
+          return false;
+     }
 
-  if(s1[i]=='}')
-  {x=a.top();
-  if(x=='{')
-  a.pop();
-  if(x!='{')
-  {return false;
-  break;}}
+    if(s1[i]=='}')
+    {
+         x=a.top();
+    if(x=='{')
+    a.pop();
+    if(x!='{')
+    {
+         return false;
+    break;
+    }
+    }
 
-   if(s1[i]==')')
-   {x=a.top();
-   if(x=='(')
-   a.pop();
-   if(x!='(')
-   {return false;
-   break;}}
+    if(s1[i]==')')
+    {
+         x=a.top();
+    if(x=='(')
+    a.pop();
+    if(x!='(')
+    {
+         return false;
+   
+         break;
+    }
+    }
 
-  if(s1[i]=='>')
-  {x=a.top();
-  if(x=='<')
-  a.pop();
-  if(x!='<')
-  {return false;
-  break;}}
+    if(s1[i]=='>')
+    {
+         x=a.top();
+         if(x=='<')
+         a.pop();
+         if(x!='<')
+         {
+              return false;
+         }
+    }
 
    if(s1[i]==']')
-   {x=a.top();
-   if(x=='[')
-   a.pop();
-   if(x!='[')
-   {return false;
-   break;}}
+   {
+        x=a.top();
+        if(x=='[')
+        a.pop();
+        if(x!='[')
+        {
+             return false;
+        }
+   }
 
 }
-return(a.empty());}
+return(a.empty());
+}
 
-
-  int main()
-  {int n,i,j;
-  cin>>n;
-  for(i=0;i<n;i++)
-  {string s;
-  cin>>s;
-  if (ismatching(s)) 
-        cout << "YES"<<endl; 
-  else
-        cout << "NO"<<endl; }
+int main()
+{
+int n,i,j;
+cin>>n;
+for(i=0;i<n;i++)
+{
+     string s;
+     cin>>s;
+     if (ismatching(s))
+     {
+          cout << "YES"<<endl;
+     }    
+     else
+     { 
+          cout << "NO"<<endl; 
+     }
 return 0;
 }
